5710640e7e2d1a0d8e24df1a1ebce69ac1071d05
MarkBelleza <bellezamark64@gmail.com>
2022-02-06 18:00:54 -0500

    camera now stay in playable area

--- Warrior/Assets/Game/Scripts/MainCamera.cs
+++ Warrior/Assets/Game/Scripts/MainCamera.cs
@@ -11,7 +11,16 @@ public class MainCamera : MonoBehaviour
     private Vector3 smoothFollow;
     private Vector3 playerPosition;
 
+    private Vector3 camPosition;
+    public float mostLeftSide;
+    public float mostRightSide;
+    public float mostBottom;
+    public float mostTop;
 
+    private void Update()
+    {
+        camPosition = transform.position;
+    }
     private void LateUpdate()
     {
         Follow();
@@ -21,9 +30,26 @@ public class MainCamera : MonoBehaviour
         //CamPosition = transform.position; //position of camera                                
         // CamPosition.x = player.position.x;
         // CamPosition.y = player.position.y + 5;
+       if (camPosition.x < mostLeftSide)
+        {
+            camPosition.x = mostLeftSide;
+        }
+        if (camPosition.x > mostRightSide)
+        {
+            camPosition.x = mostRightSide;
+        }
+        if (camPosition.y < mostBottom)
+        {
+            camPosition.y = mostBottom;
+        }
+        if (camPosition.y > mostTop)
+        {
+            camPosition.y = mostTop;
+        }
+
 
         playerPosition = player.position + offSet;
-        smoothFollow = Vector3.Lerp(transform.position, playerPosition, smoothFactor * Time.fixedDeltaTime);
+        smoothFollow = Vector3.Lerp(camPosition, playerPosition, smoothFactor * Time.fixedDeltaTime); //make camera have a slight delay when following the player
         transform.position = smoothFollow;
 
     }
